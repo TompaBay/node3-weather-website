8e8acad2f5139f4ffd3a835f68f2878cf8a2828d
Hongkai Jiang <hongkaij@andrew.cmu.edu>
2020-08-26 14:33:41 +0800

    Fix bugs for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 
-fetch('/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('/weather?address=!').then((response) => {
+//     response.json().then((data) => {
+//         if(data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -24,7 +24,7 @@ weatherForm.addEventListener('submit', (e) => {
     message_1.textContent = 'Loading...'
     message_2.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 message_1.textContent = data.error
